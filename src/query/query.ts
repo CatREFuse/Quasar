d72e82fb181c4a665c6689c1de3ecd89083b0f23
Tanshow <tanshow@Tanshows-Macbook-Pro-14.local>
2022-01-16 12:54:44 +0800

    sh

--- src/query/query.ts
+++ src/query/query.ts
@@ -1,15 +1,15 @@
-import '../model/Engine';
-import useStore from '../store/index';
-import axios from 'axios';
+import "../model/Engine";
+import useStore from "../store/index";
+import axios from "axios";
 
 export function getEngineList() {
-    axios
-        .get('https://cdn.catrefuse.com/engines.json')
-        .then((res) => {
-            useStore().engines = res.data;
-            useStore().engine = res.data[0];
-        })
-        .catch((err) => {
-            console.error(err.message);
-        });
+  axios
+    .get("https://api.catrefuse.com/assets/engines.json")
+    .then((res) => {
+      useStore().engines = res.data;
+      useStore().engine = res.data[0];
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
 }
